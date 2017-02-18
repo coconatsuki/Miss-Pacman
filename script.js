bf7160243b8af778ad0a6af350230ef9cc32c43c
Natsu <natsukichan13@hotmail.fr>
2017-02-18 13:37:10 -0500

    Fixing mousenter animation

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
-$('.body').on('mouseover', function() {
-  $(this).css({'animation':'go 10s linear forwards'});
+  var mouse_in = false;
+$('.pacman').on('mouseenter', function() {
+  $('.body').css({'animation':'go 10s linear forwards'});
   $('.food1').css({'animation': 'fadeout 0s 0.7s forwards'});
   $('.food2').css({'animation': 'fadeout 0s 1.5s forwards'});
   $('.food3').css({'animation': 'fadeout 0s 2.5s forwards'});
@@ -9,10 +10,9 @@ $('.body').on('mouseover', function() {
   $('.headtop').css({'animation': 'eatup 1s linear infinite'});
   $('.headbottom').css({'animation': 'eatdown 1s linear infinite'});
   });
-});
-$(document).ready(function() {
-$('.body').on('mouseleave', function() {
-  $(this).css({'animation':'none'});
+
+$('.pacman').on('mouseleave', function() {
+  $('.body').css({'animation':'none'});
   $('.food1').css({'animation': 'none'});
   $('.food2').css({'animation': 'none'});
   $('.food3').css({'animation': 'none'});
